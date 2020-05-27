e764ff49744b344a743a41780262bec09997b566
Jorge Sobrino Solis <j.sobrino3@usp.ceu.es>
2020-05-27 18:04:29 +0200

    Empezando el main

--- gisi-modcomputacion/src/es/ceu/gisi/modcomp/webcrawler/WebCrawler.java
+++ gisi-modcomputacion/src/es/ceu/gisi/modcomp/webcrawler/WebCrawler.java
@@ -1,5 +1,9 @@
 package es.ceu.gisi.modcomp.webcrawler;
 
+import es.ceu.gisi.modcomp.webcrawler.jflex.JFlexScraper;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Esta aplicación contiene el programa principal que ejecuta ambas partes del
  * proyecto de programación.
@@ -8,13 +12,19 @@ package es.ceu.gisi.modcomp.webcrawler;
  * @author Jorge Sobrino Solís
  */
 public class WebCrawler {
-
-    public static void main(String[] args) {
+    
+    private static String path = new java.io.File("").getAbsolutePath() + "/test/es/ceu/gisi/modcomp/webcrawler/jflex/test/";
+    private static File fichero = new File(path + "prueba1.html");
+    
+    public static void main(String[] args) throws IOException {
         // Deberá inicializar JFlexScraper con el fichero HTML a analizar
         // Y creará un fichero con todos los hiperenlaces que encuentre.
         // También deberá indicar, mediante un mensaje en pantalla que
         // el fichero HTML que se ha pasado está bien balanceado.
+        
+        JFlexScraper af = new JFlexScraper(fichero);
 
+        
         // Deberá inicializar JsoupScraper con la DIRECCIÓN HTTP de una página
         // web a analizar. Creará un fichero con todos los hiperenlaces que
         // encuentre en la página web. También obtendrá estadísticas de uso 
